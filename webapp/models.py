f643c679f518f03316c06f4f1ff9473eb4f47c0a
go-ldenkiwi <wnduq297@gmail.com>
2022-05-10 14:19:31 +0900

    모델 재수정

--- webapp/models.py
+++ webapp/models.py
@@ -4,13 +4,12 @@ from django.forms import EmailField
 
 
 class Img(models.Model):
-    create_date = models.DateTimeField()
+    icreate_date = models.DateTimeField()
     imgdata = models.ImageField()
-    imgnum = models.CharField(max_length=20)
 
 class User(models.Model):
+    ucreate_date = models.DateTimeField()
     email = models.EmailField()
     passwd = models.CharField(max_length=20)
-    uqid = models.CharField(max_length=20)
     typems = models.CharField(max_length=20)
     imgs = models.ForeignKey(Img, on_delete=models.CASCADE)
